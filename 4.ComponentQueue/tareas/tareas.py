e115dacf1eaf8f667d7bd00509bfc08c87d3c0ff
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-09-09 20:01:07 -0500

    Cambios, Servicio Alertador BKP

--- 4.ComponentQueue/tareas/tareas.py
+++ 4.ComponentQueue/tareas/tareas.py
@@ -8,37 +8,68 @@ def reintegrar_cola(temp_json):
     #AGREGA A LA COLA DE TAREAS
     registrar_temp.apply_async(args=args)
 
+def servicio_2(temp_json):
+    try:
+        content = requests.get('http://localhost:5003/healthcheck')
+        if content.status_code == 404:
+            print('SERVICIO Alertador 5003 OFFLINE------')
+            reintegrar_cola(temp_json)
+        else:
+            #print('recibido: ', content.json())
+            print('SERVICIO Alertador 5003 ONLINE')
+            requests.post('http://localhost:5003/temperatura', json=temp_json)
+            with open('log_registrar_temp.txt','a+') as file:
+                file.write('{}\n'.format(temp_json))
+    except ConnectionError as e:
+        print('SERVICIO Alertador 5003 OFFLINE')
+        reintegrar_cola(temp_json)
+    except requests.exceptions.Timeout:
+    # Maybe set up for a retry, or continue in a retry loop
+        print('SERVICIO Alertador 5003 OFFLINE --- TIMEOUT')
+        reintegrar_cola(temp_json)
+    except requests.exceptions.TooManyRedirects:
+        # Tell the user their URL was bad and try a different one
+        print('SERVICIO Alertador 5003 OFFLINE --- REDIRECT')
+        reintegrar_cola(temp_json)
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print('SERVICIO Alertador 5003 OFFLINE --- REQUEST')
+        reintegrar_cola(temp_json)
+    except Exception as e:
+        print('error' + str(e))
+        reintegrar_cola(temp_json)    
+
 @celery.task(name="registrar_temp")
 def registrar_temp(temp_json):
-    
     
     if(temp_json['temperatura'] is not None and temp_json['tipo'] is not None and temp_json['now'] is not None):
         try:
             content = requests.get('http://localhost:5002/healthcheck')
             if content.status_code == 404:
-                print('SERVICIO Alertador OFFLINE------')
-                reintegrar_cola(temp_json)
+                print('SERVICIO Alertador 5002 OFFLINE------')
+                #reintegrar_cola(temp_json)
+                servicio_2(temp_json)
             else:
                 #print('recibido: ', content.json())
-                print('SERVICIO Alertador ONLINE')
+                print('SERVICIO Alertador 5002 ONLINE')
                 requests.post('http://localhost:5002/temperatura', json=temp_json)
                 with open('log_registrar_temp.txt','a+') as file:
                     file.write('{}\n'.format(temp_json))
         except ConnectionError as e:
-            print('SERVICIO Alertador OFFLINE')
-            reintegrar_cola(temp_json)
+            print('SERVICIO Alertador 5002 OFFLINE')
+            servicio_2(temp_json)
         except requests.exceptions.Timeout:
         # Maybe set up for a retry, or continue in a retry loop
-            print('SERVICIO Alertador OFFLINE --- TIMEOUT')
-            reintegrar_cola(temp_json)
+            print('SERVICIO Alertador 5002 OFFLINE --- TIMEOUT')
+            servicio_2(temp_json)
         except requests.exceptions.TooManyRedirects:
             # Tell the user their URL was bad and try a different one
-            print('SERVICIO Alertador OFFLINE --- REDIRECT')
-            reintegrar_cola(temp_json)
+            print('SERVICIO Alertador 5002 OFFLINE --- REDIRECT')
+            servicio_2(temp_json)
         except requests.exceptions.RequestException as e:
             # catastrophic error. bail.
-            print('SERVICIO Alertador OFFLINE --- REQUEST')
-            reintegrar_cola(temp_json)
+            print('SERVICIO Alertador 5002 OFFLINE --- REQUEST')
+            servicio_2(temp_json)
         except Exception as e:
             print('error' + str(e))
         
--- 6.Component_Observador/app.py
+++ 6.Component_Observador/app.py
@@ -5,28 +5,59 @@ from datetime import datetime
 from sqlalchemy.exc import IntegrityError
 import threading
 
+def get_status():
+    try:
+        content = requests.get('http://localhost:5003/healthcheck')
+        if content.status_code == 404:
+            print('SERVICIO Alertador 5003 OFFLINE------')
+        else:
+            print('SERVICIO Alertador 5003 ONLINE')
+    except ConnectionError as e:
+        print('SERVICIO Alertador OFFLINE')
+        get_status()
+    except requests.exceptions.Timeout:
+    # Maybe set up for a retry, or continue in a retry loop
+        print('SERVICIO Alertador OFFLINE --- TIMEOUT')
+        get_status()
+    except requests.exceptions.TooManyRedirects:
+        # Tell the user their URL was bad and try a different one
+        print('SERVICIO Alertador OFFLINE --- REDIRECT')
+        get_status()
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print('SERVICIO Alertador OFFLINE --- REQUEST')
+        get_status()
+    except Exception as e:
+        print('error' + str(e))
+        get_status()
 
 def printit():
     threading.Timer(3.0, printit).start()
     try:
         content = requests.get('http://localhost:5002/healthcheck')
         if content.status_code == 404:
-            print('SERVICIO Alertador OFFLINE------')
+            print('SERVICIO Alertador 5002 OFFLINE------')
+            get_status()
         else:
             #print('recibido: ', content.json())
-            print('SERVICIO Alertador ONLINE')
+            print('SERVICIO Alertador 5002 ONLINE')
     except ConnectionError as e:
         print('SERVICIO Alertador OFFLINE')
+        get_status()
     except requests.exceptions.Timeout:
     # Maybe set up for a retry, or continue in a retry loop
         print('SERVICIO Alertador OFFLINE --- TIMEOUT')
+        get_status()
     except requests.exceptions.TooManyRedirects:
         # Tell the user their URL was bad and try a different one
         print('SERVICIO Alertador OFFLINE --- REDIRECT')
+        get_status()
     except requests.exceptions.RequestException as e:
         # catastrophic error. bail.
         print('SERVICIO Alertador OFFLINE --- REQUEST')
+        get_status()
     except Exception as e:
         print('error' + str(e))
+        get_status()
 
 printit()
